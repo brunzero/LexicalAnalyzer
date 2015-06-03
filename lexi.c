8827cf977a116ee5df23b7e37213c94b8d470fa7
Bruno Franco <brunothebeast@gmail.com>
2015-06-03 19:39:32 -0400

    Revert "V1"
    This reverts commit 7ddd196c9bc97942a84072dae9dd3bb06270d6d1.

--- lexi.c
+++ lexi.c
@@ -1,37 +1,9 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "ArrayList.h"
-char code [1000];
-FILE *codeFile;
-int takingInput();
+
 int main()
 {
-    ArrayList *stream;
 
-    stream = createArrayList(10);
-
-    put(stream, "read");
-
-    takingInput();
     return 0;
-}
-
-int takingInput()
-{
-    int i = 0;
-    int x, y;
-    codeFile = fopen("input.txt", "r");
-    if(!codeFile)
-    {
-        printf("Error in opening the file.");
-        exit(0);
-
-    }
-    while ((x = fgetc(codeFile)) != EOF)
-    {
-        code[i] = x;
-        code[i+1] = '\0';
-        i ++;
-    }
-    printf("%s", code);
 }
