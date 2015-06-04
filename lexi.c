982e403e04fb34d98ad3d18d831216ca6e1e1bd9
Bruno Franco <brunothebeast@gmail.com>
2015-06-03 20:28:43 -0400

    Fixed struct for tokens and working on reading in input

--- lexi.c
+++ lexi.c
@@ -1,16 +1,52 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include "ArrayList.h"
+
+typedef struct Token
+{
+    char *word;
+    int sym;
+}Token;
+
 char code [1000];
 FILE *codeFile;
 int takingInput();
+int structIndex = 0;
+Token *tokenArray;
+
+void structPut(char *word, int num)
+{
+    if(!tokenArray)
+    {
+        printf("Token Array not properly initialized");
+    }
+
+    Token newToken;
+    newToken.sym = num;
+    newToken.word = word;
+    tokenArray[structIndex] = newToken;
+}
+
+void initStructArray()
+{
+    tokenArray = malloc(sizeof(Token)*10000);
+}
+
 int main()
 {
+
+    initStructArray(tokenArray);
+    structPut("read", 10);
+
+    printf("%s\n\n\n\n", tokenArray[0]);
+
     ArrayList *stream;
 
     stream = createArrayList(10);
 
     put(stream, "read");
+
+
 
     takingInput();
     return 0;
